cfc1482a5b63406981859428bc981622f8979456
Andrew Prince <90168409+AndrewEPrince@users.noreply.github.com>
2023-12-01 20:11:29 +0000

    Adding to Map

---
+++ Map.h
@@ -0,0 +1,26 @@
+//TODO: Needs to Hold the rooms starting with the starting room
+//TODO: Need to create a map object(dictionary) that holds all the rooms with the key being the name of the room
+    // This will be how we will link the room doorways together
+    // Will be something like Room: west(name of room) eask(name of room) north(name of room) south(name of room)
+    // This will be how we will link the room doorways together from the text save file for the map.
+
+//TODO: could also use JSON Database to store map data.
+
+
+
+class Map
+{
+private:
+    /* data */
+public:
+    Map(/* args */);
+    ~Map();
+};
+
+Map::Map(/* args */)
+{
+}
+
+Map::~Map()
+{
+}
