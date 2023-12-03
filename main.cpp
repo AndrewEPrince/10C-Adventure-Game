0b1bd2e3d38ce9b6e3fa3cda6e152f7f27d71f1b
Caleb Clements <79200015+calebwclem@users.noreply.github.com>
2023-12-03 23:02:42 +0000

    .

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include "Room.h"
 
+#include "Menu.h"
+
+using namespace std;
+
 int main(void)
 {
-    
+    //Menu menu;
+    //menu.load();
+
+    cout << "Hello World" << endl;;
+    return 0;
 }
